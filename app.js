d6aeecbf002bc6091700b8e5e00dfbfc87ae01a6
Anandu B Ajith <reach2anandu@gmail.com>
2016-05-08 11:02:14 +0530

    add bower_components and fix firebase

--- app.js
+++ app.js
@@ -1,38 +1,6 @@
 (function(){
 
-var CANDIDATES =  [   {
-			    "_id": "572dd765f8391243e0f0df71",
-			    "name": "Prince Frye",
-			    "image": "http://loremflickr.com/150/200?random=0",
-			    "votes": 0
-			  },
-			  {
-			    "_id": "572dd765b52cb803c4eeeefb",
-			    "name": "Roberson Small",
-			    "image": "http://loremflickr.com/150/200?random=1",
-			    "votes": 0
-			  },
-			  {
-			    "_id": "572dd7650838737071c13012",
-			    "name": "Fischer Le",
-			    "image": "http://loremflickr.com/150/200?random=2",
-			    "votes": 0
-			  },
-			  {
-			    "_id": "572dd765509a123e338145a8",
-			    "name": "Sims Griffin",
-			    "image": "http://loremflickr.com/150/200?random=3",
-			    "votes": 0
-			  },
-			  {
-			    "_id": "572dd765fdbeda127387de16",
-			    "name": "Haley Levy",
-			    "image": "http://loremflickr.com/150/200?random=4",
-			    "votes": 0
-			  }
-		     ];
-
-var app = angular.module('election', ['ngRoute', 'xeditable']);
+var app = angular.module('election', ['firebase', 'ngRoute', 'xeditable']);
 
 app.config(function($routeProvider){
   $routeProvider
@@ -57,17 +25,16 @@ app.run(function(editableOptions){
   editableOptions.theme = 'bs3';
 });
 
-app.controller('mainController', function($scope) {
+app.controller('mainController', function($scope, $firebaseObject) {
   $scope.year = new Date().getFullYear();
-  $scope.candidates = CANDIDATES;
+  $scope.candidates = {};
+  var ref = new Firebase('https://school-election.firebaseio.com/candidates'),
+      syncObject = $firebaseObject(ref);
+  syncObject.$bindTo($scope, 'candidates');
 });
 
 
 app.controller('electionController', function($scope) {
-  // get all the candidates and display them
-  // when clicked add an element to the respective array
-
-
   function addVote(candidate) {
     candidate.votes = candidate.votes + 1;
     alert(candidate._id + candidate.name + candidate.votes);
@@ -86,9 +53,14 @@ app.controller('resultsController', function($scope){
 app.controller('settingsController', function($scope) {
 
   $scope.newCandidate = {};
+  $scope.newCandidate.votes = 0;
+  $scope.createForm= false;
+  var randomID  = Math.random().toString(20).slice(2);
   function addCandidate(newCandidate) {
-    candidates.push($scope.newCandidate);
-    $scope.candidate = ''
+    $scope.candidates.push({randomID: $scope.newCandidate});
+    //push to the object
+
+    $scope.newCandidate.name  = '';
   }
 
   function removeCandidate(candidate) {
@@ -96,12 +68,12 @@ app.controller('settingsController', function($scope) {
       return c._id !== candidate._id;
     });
   }
-
+  function showCreateForm() {
+    $scope.createForm = true;
+  }
+  $scope.showCreateForm = showCreateForm;
   $scope.removeCandidate = removeCandidate;
   $scope.addCandidate = addCandidate;
-  // list all the candidates
-  // should be able to edit them
-  // should be able to add new candidates
 
 });
 
