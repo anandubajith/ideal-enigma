c6e2e5418151c850260226967aaefb659df47d6c
Anandu B Ajith <reach2anandu@gmail.com>
2016-05-13 18:50:20 +0530

    voting interface - final :fire:

--- app.js
+++ app.js
@@ -1,108 +1,96 @@
-(function(){
+(function() {
 
-var app = angular.module('election', ['firebase', 'ngRoute', 'xeditable']);
+var app = angular.module('votingMachine', ['firebase', 'ngAnimate']);
 
-app.config(function($routeProvider){
-  $routeProvider
-    .when('/', {
-      templateUrl: 'pages/election.html',
-      controller: 'electionController'
-    })
-    .when('/results', {
-      templateUrl: 'pages/results.html',
-      controller: 'resultsController'
-    })
-    .when('/settings', {
-      templateUrl: 'pages/settings.html',
-      controller: 'settingsController'
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+app.run(function($rootScope){
+	$rootScope.loggedIn = false;
 });
 
-app.run(function(editableOptions){
-  editableOptions.theme = 'bs3';
-});
-
-app.controller('mainController', function($scope, $firebaseObject) {
-  $scope.year = new Date().getFullYear();
-  $scope.frozen = true;
+app.controller('mainController' , function($scope, $timeout, Loader, $firebaseObject, $firebaseArray) {
   $scope.candidates = {};
-  var ref = new Firebase('https://school-election.firebaseio.com/candidates'),
-      syncObject = $firebaseObject(ref);
-  syncObject.$bindTo($scope, 'candidates');
-});
+  $scope.modal = false;
+
+  var ref = new Firebase('https://school-election.firebaseio.com'),
+      candidatesObject = $firebaseObject(ref.child('candidates')),
+      votesArray = $firebaseArray(ref.child('votes'));
+
+  var audio = new Audio('beep.mp3');
+  candidatesObject.$bindTo($scope, 'candidates');
 
 
-app.controller('electionController', function($scope, $firebaseArray) {
 
-  var ref = new Firebase('https://school-election.firebaseio.com/votes');
-  var list = $firebaseArray(ref);
+  function showConfirm(id) {
+	$scope.modal = true;
+	Loader.show(true);
+    $scope.currentId = id;
+  }
+
+  function resetModal() {
+    $scope.modal = false;
+    $scope.currentId = 0;
+  }
+
+  function closeModal() {
+    resetModal();
+    Loader.show(false);
+  }
 
   function addVote(id) {
-    list.$add(id).then(function(ref) {
-      var id = ref.key();
-      console.log("added record with id " + id);
-      list.$indexFor(id); // returns location in the array
-    });
+    votesArray.$add(id).then(function(vote) {
+      Loader.show(true);
+	  resetModal();
 
+      $timeout(function(){
+        Loader.show(false);
+      }, 5000)
+  	
+      audio.play();
+    });
   }
+
 
   $scope.addVote = addVote;
+  $scope.resetModal = resetModal;
+  $scope.closeModal = closeModal;
+  $scope.showConfirm = showConfirm;
+ 
 });
 
 
-app.controller('resultsController', function($scope, $firebaseArray){
-  var ref = new Firebase('https://school-election.firebaseio.com/votes'),
-      list = $firebaseArray(ref),
-      results = {};
+app.controller("AuthCtrl", ["$scope","$rootScope", "$firebaseAuth",
+  function($scope, $rootScope, $firebaseAuth) {
 
-  $scope.votes = {};
-
-  list.$loaded(function(x) {
-    result = x.map(function(y){return y['$value'];});
-    console.log(result);
-    $scope.votes = result.reduce(function(count, val){
-      count[val] = (count[val] || 0 ) + 1;
-      return count;
-    }, {});
-
-  });
-
-});
-
-app.controller('settingsController', function($scope) {
-
-  $scope.newCandidate = {};
-  $scope.newCandidate.votes = 0;
-  $scope.createForm= false;
-  var randomID  = Math.random().toString(20).slice(2);
-  function addCandidate(newCandidate) {
-    $scope.candidates[randomID] = $scope.newCandidate;
-    $scope.createForm = false;
-    randomID = Math.random().toString(20).slice(2);
+    var ref = new Firebase('https://school-election.firebaseio.com');
+    auth = $firebaseAuth(ref);
+    $scope.login = function() {
+      $scope.authData = null;
+      $scope.error = null;
+      auth.$authWithPassword({email: $scope.email, password: $scope.password})
+		.then(function(authData){ $rootScope.loggedIn = true; })
+		.catch(function(err) { console.log(err);});
+    };
   }
+]);
 
-  function removeCandidate(id) {
-    delete $scope.candidates[id];
-  }
-  function toggleCreateForm() {
-    $scope.createForm = $scope.createForm ? false : true;
-  }
-  $scope.toggleCreateForm = toggleCreateForm;
-  $scope.removeCandidate = removeCandidate;
-  $scope.addCandidate = addCandidate;
+app.directive('loader', [function(){
+    return {
+      'restrict' : 'A',
+      'controller' : ['$scope', 'Loader', function($scope, Loader){
+        $scope.Loader = Loader;
+      }]
+    }
+}]);
 
-});
+app.factory('Loader', [function(){
+    var instance = {}
+    
+    instance.show = function(on){
+      instance.visible = on;
+    }
+    
+    return instance;
+}]);
 
-
-app.directive('headerBar', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'header.html'
-  };
-});
 
 
 
